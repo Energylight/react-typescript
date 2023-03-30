4a8b7eccf2ec2a281a710047b4b2a4d1ac54428c
Roman Akh <energylight87@gmail.com>
2023-03-30 23:31:04 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React, {useState} from 'react'
+import axios from 'axios'
+import {useEffect, useState} from 'react'
+import { Product } from './components/Product'
+import { IProduct } from './models'
 
-import './App.css'
+
+
 
 function App() {
+  const [products, setProducts] = useState<IProduct[]>( [] )
+  const [loading, setLoading] = useState(false)
+
+  async function fetchProducts() {
+    setLoading(true)
+    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+
+    setProducts(response.data)
+    setLoading(false)
+  }
+
+  useEffect( () => {
+    fetchProducts()
+  }, [] )
 
   return (    
     <div className='container mx-auto max-w-2xl pt-5'>
+      { loading && <p className= "text-center">Loading...</p> }
+      { products.map(product => <Product product={product} key={product.id}  /> ) }
+
+
+
       
     </div>    
   )
